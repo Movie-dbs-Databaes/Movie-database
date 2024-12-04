d6cebdba5764895d860b8b0379707999e9808ffb
Nkosinathi <136704407+NkosinathiSeptember23@users.noreply.github.com>
2024-12-04 09:04:51 +0200

    Modified search structure

--- movie-database/src/components/SearchBar.js
+++ movie-database/src/components/SearchBar.js
@@ -3,7 +3,9 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <div>
-      
+      <h1>Movie Search</h1>
+      <input type="search" placeholder="Search..."></input>
+
     </div>
   )
 }
