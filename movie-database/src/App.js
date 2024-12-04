91d8afa574fd0e377d26baef2e89e99d3cb21c37
Nkosinathi <136704407+NkosinathiSeptember23@users.noreply.github.com>
2024-12-04 09:08:56 +0200

    App.js updated

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import SearchBar from './components/SearchBar';
+import MovieDetails from './components/MovieDetails';
+import MovieList from './components/MovieList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Movie Database
+      <Router>
+      <Routes>
+        <Route path="/" element={<SearchBar />} />
+        <Route path="/MovieList" element={<MovieList />} />
+      </Routes>
+      </Router>
+       
     </div>
   );
 }
