9bc4d0eef0bbe79efc767380614fdeb563d41016
Tumisang9x <163140949+Tumisang9x@users.noreply.github.com>
2024-12-04 13:02:46 +0200

    Movielist code added

--- movie-database/src/components/MovieList.js
+++ movie-database/src/components/MovieList.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const MovieList = () => {
+const MovieList = ({ movies }) => {
   return (
-    <div>
-      
+    <div className="movie-list">
+      {movies.length > 0 ? (
+        movies.map((movie) => (
+          <div key={movie.imdbID} className="movie-card">
+            <img src={movie.Poster !== "N/A" ? movie.Poster : 'https://via.placeholder.com/200x300'} alt={movie.Title} />
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+          </div>
+        ))
+      ) : (
+        <p></p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
