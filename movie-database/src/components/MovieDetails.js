50690315b5a3802e0f0249709efd438ab8c7f6bf
Mduduzi <mduduzikevin93@gmail.com>
2025-01-07 15:22:30 +0200

    Created a function that lets the movie icons show movie details when clicked

--- movie-database/src/components/MovieDetails.js
+++ movie-database/src/components/MovieDetails.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
 
-export default MovieDetails
+  useEffect(() => {
+    const apiKey = 'e9398896e43eb1802e8c35d2e38e536c';
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => setMovie(json));
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="movie-details">
+      <h2>{movie.title}</h2>
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+      <p>{movie.overview}</p>
+      <p>Release Date: {movie.release_date}</p>
+      <p>Rating: {movie.vote_average}</p>
+    </div>
+  );
+};
+
+export default MovieDetails;
